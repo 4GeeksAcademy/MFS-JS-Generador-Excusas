66e4b9483f842d7e7090bf6e8618ab5de0c48578
MFriasSaavedra <martin.frias.saavedra@gmail.com>
2025-06-22 20:02:56 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
 };
+let quien = ['Mi perro', 'Mi hermano', 'Mi esposa', 'El gato'];
+let accion = ['se comió', 'quemó', 'aplastó', 'destrozó'];
+let que = ['mi tarea', 'mi teléfono', 'mi informe', 'mi computador'];
+let cuando = ['anoche', 'mientras dormía', 'ayer', 'el fin de semana'];
+
+function generadorexcusas(quien, accion, que, cuando) {
+  let numquien = Math.floor(Math.random() * quien.length);
+  let numaccion = Math.floor(Math.random() * accion.length);
+  let numque = Math.floor(Math.random() * que.length);
+  let numcuando = Math.floor(Math.random() * cuando.length);
+
+  return quien[numquien] + " " + accion[numaccion] + " " + que[numque] + " " + cuando[numcuando];
+}
+
+function onLoad() {
+  let excusa = document.getElementById('excuse');
+  excusa.innerHTML = generadorexcusas(quien, accion, que, cuando);
+}
+
+window.onload = onLoad;
